ae33b70777aa1852b48b0bf5bb1925d9c0be71ed
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-08-09 13:58:46 +0530

    Create Ayush's Favourite.cpp

---
+++ Ayush's Favourite.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string a;
+        char c;
+        int ans = 0;
+        cin >> a >> c;
+        for(char &i: a)ans+=c==i;
+        cout << ans << "\n";
+    }
+    return 0;
+}
