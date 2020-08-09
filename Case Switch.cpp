3df9aa4aa48d03cdf190b8cd2e9784562746b415
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-08-09 13:59:38 +0530

    Create Case Switch.cpp

---
+++ Case Switch.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string a;
+        cin >> a;
+        for(char &i: a)i = islower(i) ? toupper(i) : tolower(i);
+        cout << a << "\n";
+    }
+    return 0;
+}
